b984c975fe6e6029f2e3fb46915063018bd79f6a
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2018-09-24 11:00:28 -0300

    produtos alterar

--- app/infra/produtosBanco.js
+++ app/infra/produtosBanco.js
@@ -26,6 +26,14 @@ ProdutosBanco.prototype.salva =  function(produto,callback){
 	this.connection.query('insert into produtos set ?',produto,callback);
 }	
 
+ProdutosBanco.prototype.ver =  function(produto,callback){
+	this.connection.query('select * from produtos where id="'+produto.id+'"',callback);
+}
+
+ProdutosBanco.prototype.altera =  function(produto,callback){
+	this.connection.query('update produtos set nome="'+produto.nome+'", preco="'+produto.preco+'", descricao="'+produto.descricao+'" where id = '+produto.id,callback);
+}	
+
 module.exports = function(){
 	return ProdutosBanco;
 }
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -34,6 +34,28 @@ module.exports = function(app){
         });
         connection.end();
     })
+
+    app.get('/produtos/ver/:id?',function(req,res){
+       
+        var connection = app.infra.dbConnection();
+        var produtosBanco = new app.infra.produtosBanco(connection);
+
+        produtosBanco.ver(req.params,function(err,results,next){
+            if(err){
+                console.log('Erro no banco de dados');
+                return next(err);
+            }
+            res.format({
+                html: function(){
+                    res.render("produtos/ver",{errosValidacao:{},produtoInfo:results[0]});   
+                },
+                json: function(){
+                    res.json(results);
+                }
+            })
+        });
+        connection.end();
+    })
     
     // app.get('/produtos',listaProdutos) //posso criar uma var�vel e colocar essa fun��o dentro dessa vari�vel, caso eu for usar em mais de um lugar
         
@@ -66,6 +88,33 @@ module.exports = function(app){
         }
 
         produtosBanco.salva(dados_form,function(err,results){
+            res.redirect('/produtos');
+        })        
+    })
+
+    app.post('/produtos/ver',function(req,res){
+        var connection = app.infra.dbConnection();
+        var produtosBanco = new app.infra.produtosBanco(connection);
+        
+        //dados do post
+        var dados_form = req.body;
+        req.assert('nome','Nome é obrigatório').notEmpty();
+        req.assert('preco','Preco vazio').notEmpty();
+        req.assert('preco','Formato inválido').isFloat();
+        var erros = req.validationErrors();
+        if(erros){
+            res.format({
+                html: function(){
+                    res.status(400).render('/produtos/ver',{errosValidacao : erros, produtoInfo : dados_form});
+                },
+                json: function(){
+                    res.status(400).json(erros);
+                }
+            })
+            return false;
+        }
+
+        produtosBanco.altera(dados_form,function(err,results){
             res.redirect('/produtos');
         })        
     })
