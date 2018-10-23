f1a3b9e51ca3244596c6c927ea8d6003c4baa8d4
lucasnishimura <lucasnishimura@alphacode.com.br>
2018-10-23 19:01:17 -0300

    npm do localstorage

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,7 @@
 module.exports = function(app){
     app.get('/',function(req,res){       
+        var dados_tipo = "1";
+        window.localStorage.setItem('dados_tipo', JSON.stringify(dados_tipo))
         res.render("home/index",{lista:{}});   
     })
 }
