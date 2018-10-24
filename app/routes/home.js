787997152c587d08213d43585edb0723a98fe3b2
lucasnishimura <32456478+lucasnishimura@users.noreply.github.com>
2018-10-24 13:30:56 -0300

    reeinstalação dos pacotes

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,9 +1,5 @@
 module.exports = function(app){
     app.get('/',function(req,res){
-        if(localStorage.getItem('logado') != 'sim'){
-            res.render('coreui/login',{mensagem: 'ERRO'});
-        }else{
-            res.render("home/index",{lista:{}});   
-        }       
+        res.render("home/index",{lista:{}});   
     })
 }
